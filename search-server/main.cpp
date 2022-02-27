e2af62c6c511ef09ddcfdef3a0f0f592fac4ade4
SISHRAK <94114454+SISHRAK@users.noreply.github.com>
2022-02-27 20:53:57 +0300

    last update sprint 2

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,10 +1,34 @@
+SearchServer TestServer() {
+    const int doc_id1 = 0;
+    const int doc_id2 = 1;
+    const int doc_id3 = 2;
+    const int doc_id4 = 3;
+    const int doc_id5 = 4;
+    const int doc_id6 = 5;
 
+    const std::string content1 = "1word1 1word2 1word3 1word4";
+    const std::string content2 = "2word1 2word2 2word3 2word4";
+    const std::string content3 = "3word1 3word2 3word3 3word4 3word3 3word4";
+    const std::string content4 = "4word1 4word2 4word3 4word4";
+    const std::string content5 = "5word1 5word2 5word3 5word4 5word3 5word4";
+    const std::string content6 = "6word1 6word2 6word1 6word2";
+
+    SearchServer server;
+    server.SetStopWords("1word1 2word2 3word3");
+    server.AddDocument(doc_id1, content1, DocumentStatus::ACTUAL, { 1, 2, 3 });
+    server.AddDocument(doc_id2, content2, DocumentStatus::BANNED, { 4, 5, 6, 7, 8 });
+    server.AddDocument(doc_id3, content3, DocumentStatus::IRRELEVANT, { 1, 3, 4, 5, 6, 7, 8 });
+    server.AddDocument(doc_id4, content4, DocumentStatus::REMOVED, { 4, 5, 6, 7, 8, 20, 9 });
+    server.AddDocument(doc_id5, content5, DocumentStatus::ACTUAL, { 5, 1, 3, 4, 5, 6, 7, 8 });
+    server.AddDocument(doc_id6, content6, DocumentStatus::ACTUAL, { 9, 4, 5, 6, 7, 8, 20, 9 });
+
+    return server;
+}
 
 void TestExcludeStopWordsFromAddedDocumentContent() {
     const int doc_id = 42;
     const std::string content = "cat in the city";
     const std::vector<int> ratings = { 1, 2, 3 };
-
     {
         SearchServer server;
         server.AddDocument(doc_id, content, DocumentStatus::ACTUAL, ratings);
@@ -23,92 +47,109 @@ void TestExcludeStopWordsFromAddedDocumentContent() {
 }
 
 void TestAddingDocuments() {
-    const int doc_id1 = 0;
-    const int doc_id2 = 1;
-    const int doc_id3 = 2;
-    const int doc_id4 = 3;
+    SearchServer server = TestServer();
 
-    const std::string content1 = "1word1 1word2 1word3 1word4";
-    const std::string content2 = "2word1 2word2 2word3 2word4";
-    const std::string content3 = "3word1 3word2 3word3 3word4 3word3 3word4";
-    const std::string content4 = "4word1 4word2 4word3 4word4";
+    const std::vector<Document>& fd0 = server.FindTopDocuments("word1");
+    ASSERT_HINT(fd0.empty(), "word doesn't exist");
 
-    const std::vector<int> ratings = { 1, 2, 3 };
-    {
-        SearchServer server;
-        const std::vector<Document>& found_documents = server.FindTopDocuments("word1");
-        ASSERT(found_documents.empty());
-    }
+    const std::vector<Document>& fd1 = server.FindTopDocuments("1word2");
+    const std::vector<Document>& fd2 = server.FindTopDocuments("2word2", DocumentStatus::BANNED);
+    const std::vector<Document>& fd3 = server.FindTopDocuments("1word2");
 
-    {
-        SearchServer server;
-        server.SetStopWords("stop1 stop2 stop3");
-        const std::vector<Document>& fd = server.FindTopDocuments("word2");
-        const std::vector<Document>& fds = server.FindTopDocuments("word2", DocumentStatus::BANNED);
-        const std::vector<Document>& fdsp = server.FindTopDocuments("word2", [](const int id, const DocumentStatus ds, int rating) {return rating > 1; });
-        ASSERT(fd.empty());
-        ASSERT(fds.empty());
-        ASSERT_HINT(fdsp.empty(), "vector must be empty");
-        ASSERT_EQUAL_HINT(server.GetDocumentCount(), 0, "Documents count == 0");
-    }
+    ASSERT(fd1.size() == 1);
+    ASSERT(fd2.empty());       
+    ASSERT_EQUAL(fd3[0].id, 0);
+    ASSERT_EQUAL_HINT(server.GetDocumentCount(), 6, "Documents count == 6");
+}
 
-    {
-        SearchServer server;
-        server.SetStopWords("1word1 2word2 2word3");
-        server.AddDocument(doc_id1, content1, DocumentStatus::ACTUAL, { 1, 2, 3 });
-        server.AddDocument(doc_id2, content2, DocumentStatus::BANNED, { 4, 5, 6, 7, 8 });
-        server.AddDocument(doc_id3, content3, DocumentStatus::IRRELEVANT, { 1, 3, 4, 5, 6, 7, 8 });
-        server.AddDocument(doc_id4, content4, DocumentStatus::REMOVED, { 4, 5, 6, 7, 8, 20, 9 });
+void TestStopWords(){
+    SearchServer server = TestServer();
 
-        {
-            const std::vector<Document>& fd = server.FindTopDocuments("1word2");
-            ASSERT_EQUAL(fd[0].id, doc_id1);
-        }
+    const std::vector<Document>& fd = server.FindTopDocuments("2word2");
+    ASSERT_HINT(fd.empty(), "vector must be empty");
 
-        {
-            const std::vector<Document>& fd = server.FindTopDocuments("2word2");
-            ASSERT_HINT(fd.empty(), "vector must be empty");
-        }
+}
+void TestMinusWords() {
+    SearchServer server = TestServer();
 
-        {
-            const std::vector<Document>& fd1 = server.FindTopDocuments("1word2 -1word3");
-            ASSERT(fd1.empty());
-            const std::vector<Document>& fd2 = server.FindTopDocuments("2word1 -1word3", DocumentStatus::BANNED);
-            ASSERT(fd2[0].id == doc_id2);
-        }
+    const std::vector<Document>& fd1 = server.FindTopDocuments("1word2 -1word3");
+    ASSERT(fd1.empty());
+    const std::vector<Document>& fd2 = server.FindTopDocuments("2word1 -1word3", DocumentStatus::BANNED);
+    ASSERT_EQUAL(fd2[0].id, 1);
 
-        {
-            const auto& [w1, ds1] = server.MatchDocument("1word1 1word2 2word1 2word2", doc_id1);
-            const auto& [w2, ds2] = server.MatchDocument("1word1 1word2 2word1 2word2", doc_id2);
-            const auto& [w3, ds3] = server.MatchDocument("1word1 1word2 -2word1 2word2", doc_id1);
-            const auto& [w4, ds4] = server.MatchDocument("-1word2 -2word1 2word2", doc_id1);
-            ASSERT(w1[0] == "1word2"); ASSERT(ds1 == DocumentStatus::ACTUAL);
-            ASSERT(w2[0] == "2word1"); ASSERT(ds2 == DocumentStatus::BANNED);
-            ASSERT(w3[0] == "1word2"); ASSERT(ds3 == DocumentStatus::ACTUAL);
-            ASSERT(w4.empty());
-        }
+}
+void TestMatchingDocuments() {
+    SearchServer server = TestServer();
 
-    }
+    const auto& [w1, ds1] = server.MatchDocument("1word1 1word2 2word1 2word2", 0);
+    const auto& [w2, ds2] = server.MatchDocument("1word1 1word2 2word1 2word2", 1);
+    const auto& [w3, ds3] = server.MatchDocument("1word1 1word2 -2word1 2word2", 0);
+    const auto& [w4, ds4] = server.MatchDocument("-1word2 -2word1 2word2", 0);
 
-    {
-        SearchServer server;
-        server.SetStopWords("1word1 2word2 2word3");
-        server.AddDocument(doc_id1, content1, DocumentStatus::ACTUAL, { 1, 2, 3 });
-        server.AddDocument(doc_id2, content2, DocumentStatus::ACTUAL, { 4, 5, 6, 7, 8 });
-        server.AddDocument(doc_id3, content3, DocumentStatus::ACTUAL, { 1, 3, 4, 5, 6, 7, 8 });
-        server.AddDocument(doc_id4, content4, DocumentStatus::ACTUAL, { 4, 5, 6, 7, 8, 20, 9 });
+    ASSERT(w1[0] == "1word2"); ASSERT(ds1 == DocumentStatus::ACTUAL);
+    ASSERT(w2[0] == "2word1"); ASSERT(ds2 == DocumentStatus::BANNED);
+    ASSERT(w3[0] == "1word2"); ASSERT(ds3 == DocumentStatus::ACTUAL);
+    ASSERT(w4.empty());
 
-        const std::vector<Document>& fd = server.FindTopDocuments("1word2 -2word1 3word1 3word2 3word3 4word1");
-        ASSERT(fd[1].relevance < fd[0].relevance); ASSERT(fd[0].rating < fd[2].rating);
-        ASSERT(fd[2].relevance < fd[1].relevance); ASSERT(fd[1].rating < fd[0].rating);
-        ASSERT(fd[0].relevance == 0.92419624074659368); ASSERT(fd[0].rating == 4);
+}
+void TestSortByRelevance() {
+    SearchServer server = TestServer();
+    const std::string query = "1word2 2word1 3word1 3word2 3word3 4word1 5word5 5word2 6word3 6word1";
+    const std::vector<Document>& fd = server.FindTopDocuments(query);
+    ASSERT(fd[1].relevance < fd[0].relevance);
+    ASSERT(fd[2].relevance < fd[1].relevance);
+    ASSERT(fd[1].relevance < fd[0].relevance);      
+    ASSERT(fd[2].relevance < fd[1].relevance);      
+}
+void TestOfRating() {
+    SearchServer server = TestServer();
+    const std::string query = "-1word2 -2word1 3word1 3word2 3word3 4word1 5word5 5word2 6word3 6word1";
+    const std::vector<Document>& fd = server.FindTopDocuments(query);
 
-    }
+    ASSERT_EQUAL(fd[0].rating, 8);
+    ASSERT_HINT(fd[1].rating < fd[0].rating, "");
+}
+void TestSortingByPredicate() {
+    SearchServer server = TestServer();
+    const std::string query = "-1word2 -2word1 3word1 3word2 3word3 4word1 5word5 5word2 6word3 6word1";
+
+    const std::vector<Document>& fdsp = server.FindTopDocuments(query, [](const int id, const DocumentStatus ds, int rating) {return rating < 8 && 2 < rating; });
+
+    ASSERT_EQUAL_HINT(fdsp.size(), 2, "docs with rating 4");
+}
+
+void TestByStatus() {
+    SearchServer server = TestServer();
+    const std::string query = "-1word2 -2word1 3word1 3word2 3word3 4word1 5word5 5word2 6word3 6word1";
+    const std::vector<Document>& fdA = server.FindTopDocuments(query, DocumentStatus::ACTUAL);
+    const std::vector<Document>& fdB = server.FindTopDocuments(query, DocumentStatus::BANNED);
+    const std::vector<Document>& fdI = server.FindTopDocuments(query, DocumentStatus::IRRELEVANT);
+    const std::vector<Document>& fdR = server.FindTopDocuments(query, DocumentStatus::REMOVED);
+
+    ASSERT_EQUAL(fdA.size(), 2);
+    ASSERT(fdB.empty());
+    ASSERT_EQUAL(fdI.size(), 1);
+    ASSERT_EQUAL(fdR.size(), 1);
+}
+
+void TestOfRelevance() {
+    SearchServer server = TestServer();
+    const std::vector<Document>& fd = server.FindTopDocuments("-1word2 -2word1 3word1 3word2 3word3 4word1 5word5 5word2 6word3 6word1");
+    ASSERT_EQUAL(fd[0].relevance, 0.89587973461402748);
+    ASSERT_EQUAL(fd[1].relevance, 0.29862657820467581);
 }
 
 void TestSearchServer() {
     RUN_TEST(TestExcludeStopWordsFromAddedDocumentContent);
     RUN_TEST(TestAddingDocuments);
+    RUN_TEST(TestStopWords);
+    RUN_TEST(TestMinusWords);
+    RUN_TEST(TestMatchingDocuments);
+    RUN_TEST(TestSortByRelevance);
+    RUN_TEST(TestOfRating);
+    RUN_TEST(TestSortingByPredicate);
+    RUN_TEST(TestByStatus);
+    RUN_TEST(TestOfRelevance);
 }
 
 
