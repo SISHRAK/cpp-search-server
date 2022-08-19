82a66882422ebdebd7d8bf9c59a99e606a826625
SISHRAK <94114454+SISHRAK@users.noreply.github.com>
2022-08-19 16:50:51 +0300

    Update search_server.h

--- search-server/Sprint-5/search_server.h
+++ search-server/Sprint-5/search_server.h
@@ -14,6 +14,7 @@
 #include "string_processing.h"
 
 const int MAX_RESULT_DOCUMENT_COUNT = 5;
+const double P = 1e-6;
 
 class SearchServer {
 public:
@@ -93,7 +94,7 @@ std::vector<Document> SearchServer::FindTopDocuments(const std::string& raw_quer
     auto matched_documents = FindAllDocuments(query, document_predicate);
 
     sort(matched_documents.begin(), matched_documents.end(), [](const Document& lhs, const Document& rhs) {
-        if (std::abs(lhs.relevance - rhs.relevance) < 1e-6) {
+        if (std::abs(lhs.relevance - rhs.relevance) < P) {
             return lhs.rating > rhs.rating;
         } else {
             return lhs.relevance > rhs.relevance;
